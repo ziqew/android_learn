20b48f4c84d370796bbfb1cad3eee4d66c9dbd4d
ziqew <gongwenwei@gmail.com>
2013-05-19 21:04:29 +0800

    add hack fifty

---
+++ app_demo/src/com/appdemo/fiftyhacks/Hack01Activity.java
@@ -0,0 +1,15 @@
+package com.appdemo.fiftyhacks;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.appdemo.R;
+
+public class Hack01Activity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hack01);
+    }
+}
---
+++ app_demo/src/com/appdemo/fiftyhacks/Hack02Activity.java
@@ -0,0 +1,11 @@
+package com.appdemo.fiftyhacks;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gongwenwei
+ * Date: 5/19/13
+ * Time: 8:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hack02Activity {
+}
