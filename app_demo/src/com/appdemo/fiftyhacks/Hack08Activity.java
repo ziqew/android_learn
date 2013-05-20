101f7da60a1a61fda81effebc5da992e1cff3d96
ziqew <gongwenwei@gmail.com>
2013-05-20 21:21:40 +0800

    add hack fifty

---
+++ app_demo/src/com/appdemo/fiftyhacks/Hack08Activity.java
@@ -0,0 +1,11 @@
+package com.appdemo.fiftyhacks;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gongwenwei
+ * Date: 5/20/13
+ * Time: 8:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hack08Activity {
+}
